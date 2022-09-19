d8ecf4be5fca9d8411014f0ecb670e8820ec108c
Черных Павел <santiniel@gmail.com>
2022-09-19 15:23:42 +0300

    Добавлена возможность отображать услуги на соответствующей странице. Размечены основные кнопки приложения

---
+++ SNT WEB/Controllers/ServicesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SNT_WEB.Domain;
+using System;
+
+namespace SNT_WEB.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly DataManager dataManager;
+
+        public ServicesController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Index(Guid id)
+        {
+            if (id != default)
+            {
+                return View("Show", dataManager.ServiceItems.GetServiceItemById(id));
+            }
+
+            ViewBag.TextField = dataManager.TextField.GetTextFieldCodeWord("PageServices");
+            return View(dataManager.ServiceItems.GetServiceItem());
+        }
+    }
+}
---
+++ SNT WEB/Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SNT_WEB.Domain;
+using System.Threading.Tasks;
+
+namespace SNT_WEB.Models.ViewComponents
+{
+    public class SidebarViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public SidebarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.ServiceItems.GetServiceItem()));
+        }
+    }
+}
